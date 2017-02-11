98f8b214c7764baef85e8cf03017771bc8c22f6c
kndllbrwn <kndllbrwn1914@gmail.com>
2017-02-10 19:38:56 -0500

    Trying to fix production bug

--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,13 @@
 var socket = io();
 
-socket.on('connect', function() {
-    // console.log("Connected to server");
-
-    // socket.emit('createEmail', {
-    //     to: 'jen@example.com',
-    //     text: 'Hey. This is Andrew'
-    // });
-
-    // socket.emit('createMessage', {
-    //     from: 'kb3',
-    //     text: 'Hi, I\'m writing from the server'
-    //     });
+socket.on('connect', function () {
+  console.log('Connected to server');
 });
 
-socket.on('newMessage', function(message) {
-    console.log("Message from:", message.from);
-    console.log("Message:", message.text);
-    console.log("Created at:", message.createdAt);
-}); 
-
-socket.on('disconnect', function() {
-    console.log("Disconnected from server");
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
 });
 
-// socket.on('newEmail', function(email){
-//     console.log("New email", email);
-// });
-
-    
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+});
--- server/server.js
+++ server/server.js
@@ -1,48 +1,50 @@
-const express = require('express');
-const http = require('http');
 const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-const socketIO = require('socket.io');
-var app = express();
 const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
+var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
+app.use(express.static(publicPath));
+
 io.on('connection', (socket) => {
-    console.log('New user connected');
+  console.log('New user connected');
 
-    // socket.emit('newEmail', {
-    //     from: 'mike@example.com',
-    //     text: 'Hey. What is going on',
-    //     createdAt: 123
-    // });
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
 
-    // socket.emit('newMessage', {
-    //     from: 'mike@example.com',
-    //     text: 'Hey. What is going on',
-    //     createdAt: 123
-    // });    
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
 
-    socket.on('createMessage', (message) => {
-        console.log('Message received from:', message.from);
-        console.log('Message:', message.text);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
     });
-
-    // socket.on('createEmail', (newEmail) => {
-    //     console.log('Create email', newEmail);
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
     // });
-    socket.on('disconnect', (socket) => {
-        console.log('User was disconnected');
-    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
 });
 
 server.listen(port, () => {
-  console.log(`Started up at port ${port}`);
+  console.log(`Server is up on ${port}`);
 });
